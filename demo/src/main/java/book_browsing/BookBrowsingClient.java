2953dcbe28d9bc4cc68a2a781e52a9e38ebf1cba
GreatKingPlatypus1 <jquintana20052019@gmail.com>
2026-03-07 17:38:51 -0500

    Add frontend client for book browsing by genre

---
+++ demo/src/main/java/book_browsing/BookBrowsingClient.java
@@ -0,0 +1,33 @@
+package BookBrowsing;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class BookBrowsingClient {
+    public static void getBooksByGenre(String genre) {
+        String url = "http://localhost:8080/api/books/genre/" + genre;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        getBooksByGenre("Programming");
+    }
+}
