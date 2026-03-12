d7088f32f422d87563f349beb9b85dbd94ab5c2c
GreatKingPlatypus1 <jquintana20052019@gmail.com>
2026-03-12 19:25:53 -0400

    Add remaining book browsing client methods

--- demo/src/main/java/book_browsing/BookBrowsingClient.java
+++ demo/src/main/java/book_browsing/BookBrowsingClient.java
@@ -7,10 +7,13 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class BookBrowsingClient {
-    public static void getBooksByGenre(String genre) {
-        String url = "http://localhost:8080/api/books/genre/" + genre;
 
-        HttpClient client = HttpClient.newHttpClient();
+    private static final String BASE_URL = "http://localhost:8080/api/books";
+    private static HttpClient client = HttpClient.newHttpClient();
+
+    public static void getBooksByGenre(String genre) {
+
+        String url = BASE_URL + "/genre/" + genre;
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
@@ -20,6 +23,8 @@ public class BookBrowsingClient {
         try {
             HttpResponse<String> response =
                     client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Books for genre: " + genre);
             System.out.println(response.body());
 
         } catch (IOException | InterruptedException e) {
@@ -27,7 +32,66 @@ public class BookBrowsingClient {
         }
     }
 
-    public static void main(String[] args) {
-        getBooksByGenre("Programming");
+    public static void getTopSellers() {
+
+        String url = BASE_URL + "/top-sellers";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Top selling books:");
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getBooksByRating(double rating) {
+
+        String url = BASE_URL + "/rating/" + rating;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Books with rating >= " + rating);
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void discountBooksByPublisher(String publisher, double percent) {
+
+        String url = BASE_URL + "/discount/" + publisher + "/" + percent;
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .PUT(HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        try {
+            HttpResponse<String> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            System.out.println("Discount applied to publisher: " + publisher);
+            System.out.println(response.body());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
