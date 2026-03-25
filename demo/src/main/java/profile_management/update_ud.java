18aa5eec093d0380c25fd4edfc0b61e546bd1974
Lambsauce1919 <32989326+Lambsauce1919@users.noreply.github.com>
2026-03-25 11:14:19 -0400

    Update update_ud.java

--- demo/src/main/java/profile_management/update_ud.java
+++ demo/src/main/java/profile_management/update_ud.java
@@ -14,14 +14,14 @@ public class update_ud {
 	
 	public static void update_information(Scanner sc) {
 		
-		System.out.println("\nInformation Update Manager:\nu - username\np - password\nn - name\nh - home address"
+		System.out.println("\nInformation Update Manager:\nu - email\np - password\nn - name\nh - home address"
 				+ "\n");
 		
 		String inp = sc.nextLine();
 			switch (inp) {
 			
 			case"u":
-				System.out.println("Enter your newly desired username: ");
+				System.out.println("Enter your newly desired email: ");
 				inp = sc.nextLine();
 				sendPayload(1, inp);
 				break;
