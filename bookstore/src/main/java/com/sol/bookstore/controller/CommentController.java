7aa2e3d1004c1fb5cc0a2a2dbead5a6a6f5159de
Sol <taemrawlins@gmail.com>
2026-03-29 17:01:31 -0400

    can u see this

--- bookstore/src/main/java/com/sol/bookstore/controller/CommentController.java
+++ bookstore/src/main/java/com/sol/bookstore/controller/CommentController.java
@@ -1,11 +1,19 @@
 package com.sol.bookstore.controller;
 
 import com.sol.bookstore.Commenting;
+import com.sol.bookstore.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
 
     @RestController
     @RequestMapping("/comments")
     public class CommentController {
+
+        private List<Commenting> comments = new ArrayList<>();
+        @Autowired
+        private CommentRepository commentRepository;
 
         @PostMapping
         public void createComment(
@@ -16,6 +24,19 @@ import org.springframework.web.bind.annotation.*;
             Commenting newComment = new Commenting(comment, userID, bookID);
 
             System.out.println("Comment created for book " + bookID);
+        }
+        @GetMapping
+        public List<Commenting> getCommentsByBook(@RequestParam int bookID) {
+
+            List<Commenting> result = new ArrayList<>();
+
+            for (Commenting c : comments) {
+                if (c.bookID == bookID) {
+                    result.add(c);
+                }
+            }
+
+            return result;
         }
     }
 
--- bookstore/src/main/java/com/sol/bookstore/repository/CommentRepository.java
+++ bookstore/src/main/java/com/sol/bookstore/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.sol.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.sol.bookstore.Commenting;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.sol.bookstore.repository.CommentRepository;
 
 public interface CommentRepository extends JpaRepository<Commenting, Long> {
 
